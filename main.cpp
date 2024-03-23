683a7a1720a58fafcf3bef7f10ecafe2643f3532
lghdnov <zloyhomichok@gmail.com>
2024-03-23 16:19:40 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Money{
+private:
+    int nominal, count;
+
+public:
+    Money(int _nominal, int _count):
+    nominal(_nominal),
+    count(_count)
+    {}
+
+    string toString() const{
+        ostringstream s_stream;
+        s_stream
+            << "nominal: " << nominal << " "
+            << "count: " << count;
+
+        return s_stream.str();
+    }
+
+    int getSum() const{
+        return nominal * count;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Money money(10, 10);
+    cout << money.toString();
 }
