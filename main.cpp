ddc43005b751fed72a90d1b0911867b7305752f3
lghdnov <zloyhomichok@gmail.com>
2024-03-23 16:37:06 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Date{
+private:
+    int day, month, year;
+public:
+    Date(): day(0), month(0), year(0) {}
+
+    Date(int _day, int _month, int _year):
+    day(_day), month(_month), year(_year) {}
+
+    ~Date(){
+        cout << "BABAHH!" << endl;
+    }
+
+    string toString() const{
+        stringstream s_stream;
+        s_stream
+            << "day: " << day << " "
+            << "month: " << month << " "
+            << "year: " << year;
+
+        return s_stream.str();
+    }
+
+    bool isLeap() const{
+        return year % 4 == 0;
+    }
+
+    void increaseByFive(){
+        day += 5;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
